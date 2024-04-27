7e7c128d4a77a7b35ff2869c24b7c9d940bcf18c
genchadt <62498018+genchadt@users.noreply.github.com>
2024-04-27 10:11:40 -0400

    Initial commit

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,11 @@
+-- Remove the `use` here if you're using folke/lazy.nvim.
+return {
+  "Exafunction/codeium.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({})
+  end,
+}
